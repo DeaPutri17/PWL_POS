db20cf8381b4d2bca11f7438c4302a6cc7bfdbd3
DeaPutri17 <putrinastitidea@gmail.com>
2024-03-09 16:20:53 +0700

    Implementasi Eloquent ORM

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\UserModel;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    
+    public function index(){
+        //tambah data user dengan Eloquent Model
+        // $data = [
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 2
+        // ];
+        // UserModel::insert($data); //tambahkan data ke tabel m_user
+        $data = [
+            'nama' => 'Manager ke-3',
+        ];
+        UserModel::where('username', 'manager_tiga')->update($data);//Update data user
+
+        //coba akses model UserModel
+        $user = userModel::all(); //ambil semua data dari tabel m_user
+        return view('user', ['data'=>$user]);
+    }
+    
+}
