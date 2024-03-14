c7d0f9fa18bedcbb582f0f6e7046abb76c889902
DeaPutri17 <putrinastitidea@gmail.com>
2024-03-14 17:54:57 +0700

    not found exception

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\UserModel;
 use Illuminate\Support\Facades\Hash;
-
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 class UserController extends Controller
 {
     
@@ -38,9 +38,11 @@ class UserController extends Controller
         // $user = UserModel::firstWhere('level_id', 1);
         //coba akses model UserModel
         //$user = userModel::all(); //ambil semua data dari tabel m_user
-        $user = UserModel::findOr(20, ['username', 'nama'], function(){
-            abort(404);
-        });
+        // $user = UserModel::findOr(20, ['username', 'nama'], function(){
+        //     abort(404);
+        // });
+        //$user = UserModel::findOrFail(1);
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data'=>$user]);
     }
     
