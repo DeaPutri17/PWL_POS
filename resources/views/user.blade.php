2765e0a653926e5901f71f84f2e9939a213c85da
DeaPutri17 <putrinastitidea@gmail.com>
2024-03-14 21:13:43 +0700

    Retreiving or Creating Models

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -7,11 +7,18 @@
         <h1>Data User</h1>
         <table border="1" cellpadding="2" cellspacing="0">
             <tr>
-                {{-- <th>ID</th>
+                <th>ID</th>
                 <th>Username</th>
                 <th>Nama</th>
-                <th>ID Level Pengguna</th> --}}
-                <th>Jumlah Pengguna</th>
+                <th>ID Level Pengguna</th>
+                {{-- <th>Jumlah Pengguna</th> --}}
+            </tr>
+            <tr>
+                <td>{{ $data->user_id }}</td>
+                <td>{{ $data->username }}</td>
+                <td>{{ $data->nama }}</td>
+                <td>{{ $data->level_id }}</td>
+                {{-- <td>{{ $data }} --}}
             </tr>
             {{-- @foreach ($data as $d)
             <tr>
@@ -21,13 +28,7 @@
                 <td>{{ $d->level_id }}</td>
             </tr>
             @endforeach --}}
-            <tr>
-                {{-- <td>{{ $data->user_id }}</td>
-                <td>{{ $data->username }}</td>
-                <td>{{ $data->nama }}</td>
-                <td>{{ $data->level_id }}</td> --}}
-                <td>{{ $data }}
-            </tr>
+
         </table>
     </body>
 </html>
