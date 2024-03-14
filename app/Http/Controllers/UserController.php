59d0f2a399584b6d498b098abccf6ea3062078d4
DeaPutri17 <putrinastitidea@gmail.com>
2024-03-14 17:40:27 +0700

    Retrieving Single Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -23,17 +23,24 @@ class UserController extends Controller
         // ];
         // UserModel::where('username', 'manager_tiga')->update($data);//Update data user
 
-        $data = [
-            'level_id' => 2,
-            //'username' => 'manager_dua',
-            //'nama' => 'Manager 2',
-            'username' => 'manager_empat',
-            'nama' => 'Manager 3    ',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
+        // $data = [
+        //     'level_id' => 2,
+        //     //'username' => 'manager_dua',
+        //     //'nama' => 'Manager 2',
+        //     'username' => 'manager_empat',
+        //     'nama' => 'Manager 3    ',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data);
+
+        //$user = UserModel::find(1);
+        //$user = UserModel::where('level_id', 1)->first();
+        // $user = UserModel::firstWhere('level_id', 1);
         //coba akses model UserModel
-        $user = userModel::all(); //ambil semua data dari tabel m_user
+        //$user = userModel::all(); //ambil semua data dari tabel m_user
+        $user = UserModel::findOr(20, ['username', 'nama'], function(){
+            abort(404);
+        });
         return view('user', ['data'=>$user]);
     }
     
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -10,7 +10,7 @@ class UserModel extends Model
     use HasFactory;
 
     protected $table = 'm_user'; //Mendefinisikan nama tabel yang digunakan oleh model ini
-    protected $primarykey = 'user_id'; //mendefinisikan primary key dari tabel yang dignakan
+    protected $primaryKey = 'user_id'; //mendefinisikan primary key dari tabel yang dignakan
     //protected $fillable = ['level_id', 'username', 'nama', 'password'];
     protected $fillable = ['level_id', 'username', 'nama', 'password'];
 }
