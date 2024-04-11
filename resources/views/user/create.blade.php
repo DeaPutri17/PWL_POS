e01b84883103f88d7f4c0f83b85060e86c4543ff
DeaPutri17 <putrinastitidea@gmail.com>
2024-04-11 11:57:29 +0700

    praktikum 3

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -1,68 +1,67 @@
-@extends('layouts.app')
-{{-- Customize layout sections --}}
-@section('subtitle', 'User')
-@section('content_header_title', 'User')
-@section('content_header_subtitle', 'Create')
-{{-- Content body: main page content --}}
+@extends('layouts.template')
+
 @section('content')
-    <div class="container">
-        <div class="card card-primary">
-            <div class="card-header">
-            <h3 class="card-title">Buat User baru</h3>
+    <div class="card card-outline card-primary">
+        <div class="card-header">
+            <h3 class="card-title">{{ $page->title }}</h3>
+            <div class="card-tools"></div>
             </div>
-
-            <form method="post" action="../user">
-                <div class="card-body">
-                    <div class="form-group">
-                        <label for="usernameUser">Username</label>
-                        <input id="username"
-                            type="text" 
-                            name="username"
-                            class="@error('username') is-invalid @enderror"  placeholder="Masukkan Username">
-
-                            @error('username')
-                                <div class="alert alert-danger">{{ $message }}</div>
-                            @enderror
-                    </div>
-                    <div class="form-group">
-                        <label for="namaUser">Nama</label>
-                        <input id="nama"
-                            type="text" 
-                            name="nama"
-                            class="@error('nama') is-invalid @enderror"  placeholder="Masukkan Nama">
-
-                            @error('nama')
-                                <div class="alert alert-danger">{{ $message }}</div>
-                            @enderror
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input id="password"
-                            type="text" 
-                            name="password"
-                            class="@error('password') is-invalid @enderror"  placeholder="Masukkan Password">
-
-                            @error('password')
-                                <div class="alert alert-danger">{{ $message }}</div>
-                            @enderror
-                    </div>
-                    <div class="form-group">
-                        <label for="idLevel">ID Level</label>
-                        <input id="level_id"
-                            type="text" 
-                            name="level_id"
-                            class="@error('level_id') is-invalid @enderror"  placeholder="Masukkan Level ID">
-
-                            @error('level_id')
-                                <div class="alert alert-danger">{{ $message }}</div>
-                            @enderror
+            <div class="card-body">
+                <form method="POST" action="{{ url('user') }}" class="form-horizontal">
+                @csrf
+                <div class="form-group row">
+                    <label class="col-1 control-label col-form-label">Level</label>
+                    <div class="col-11">
+                    <select class="form-control" id="level_id" name="level_id" required>
+                        <option value="">- Pilih Level -</option>
+                        @foreach($level as $item)
+                            <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
+                        @endforeach
+                    </select>
+                    @error('level_id')
+                        <small class="form-text text-danger">{{ $message }}</small>
+                    @enderror
                     </div>
                 </div>
-
-                <div class="card-footer">
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <div class="form-group row">
+                    <label class="col-1 control-label col-form-label">Username</label>
+                    <div class="col-11">
+                        <input type="text" class="form-control" id="username" name="username" value="{{ old('username') }}" required>
+                        @error('username')
+                            <small class="form-text text-danger">{{ $message }}</small>
+                        @enderror
+                    </div>
                 </div>
-            </form>
-        </div>  
+                <div class="form-group row">
+                    <label class="col-1 control-label col-form-label">Nama</label>
+                    <div class="col-11">
+                        <input type="text" class="form-control" id="nama" name="nama" value="{{ old('nama') }}" required>
+                        @error('nama')
+                            <small class="form-text text-danger">{{ $message }}</small>
+                        @enderror
+                    </div>
+                </div>
+                <div class="form-group row">
+                <label class="col-1 control-label col-form-label">Password</label>
+                    <div class="col-11">
+                    <input type="password" class="form-control" id="password" name="password" required>
+                        @error('password')
+                            <small class="form-text text-danger">{{ $message }}</small>
+                        @enderror
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-1 control-label col-form-label"></label>
+                        <div class="col-11">
+                        <button type="submit" class="btn btn-primary btn-sm">Simpan</button>
+                            <a class="btn btn-sm btn-default ml-1" href="{{ url('user') }}">Kembali</a>
+                        </div>
+                </div>
+                </form>
+            </div>
     </div>
 @endsection
+@push('css')
+@endpush
+@push('js')
+@endpush
--- routes/web.php
+++ routes/web.php
@@ -39,12 +39,12 @@ Route::get('/kategori/edit/{id}', [KategoriController::class, 'edit']);
 Route::put('/kategori/{id}', [KategoriController::class, 'storeEdit']);
 Route::get('/kategori/hapus/{id}', [KategoriController::class, 'hapus'])->name('kategori.hapus');
 //Manage User
-Route::get('/user/create', [UserController::class, 'create']);
-Route::post('/user', [UserController::class, 'store']);
-Route::get('/user', [UserController::class, 'index']);
-Route::get('/user/edit/{id}', [UserController::class, 'edit']);
-Route::put('/user/{id}', [UserController::class, 'storeEdit']);
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+// Route::get('/user/create', [UserController::class, 'create']);
+// Route::post('/user', [UserController::class, 'store']);
+// Route::get('/user', [UserController::class, 'index']);
+// Route::get('/user/edit/{id}', [UserController::class, 'edit']);
+// Route::put('/user/{id}', [UserController::class, 'storeEdit']);
+// Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
 //Manage Level
 Route::get('/level/create', [LevelController::class, 'create']);
 Route::post('/level', [LevelController::class, 'store']);
@@ -56,3 +56,14 @@ Route::get('/level/hapus/{id}', [LevelController::class, 'hapus']);
 Route::resource('m_user', POSController::class);
 
 Route::get('/', [WelcomeController::class, 'index']);
+
+Route::group(['prefix' => 'user'], function(){
+    Route::get('/', [UserController::class, 'index']);          //menampilkan halaman awal user
+    Route::post('/list', [UserController::class, 'list']);      //menampilkan data user dalam bentuk json untuk datatables
+    Route::get('/create', [UserController::class, 'create']);   //menampilkan halaman form tambah user
+    Route::post('/', [UserController::class, 'store']);         //menyimpan data user baru
+    Route::get('/{id}', [UserController::class, 'show']);       //menampilkan detail user
+    Route::get('/{id}/edit', [UserController::class, 'edit']);  //menampilkan halaman form edit user
+    Route::put('/{id}', [UserController::class, 'update']);     //menyimpan perubahan data user
+    Route::delete('/{id}', [UserController::class, 'destroy']); //menghapus data user
+});
