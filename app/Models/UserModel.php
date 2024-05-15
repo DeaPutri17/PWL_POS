9d59f7197f1188be4aa8884358894b053af6d6e0
DeaPutri17 <putrinastitidea@gmail.com>
2024-05-15 13:04:00 +0700

    Praktikum 1 – Implementasi Eloquent Accessor

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Database\Eloquent\Casts\Attribute; 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class UserModel extends Authenticatable implements JWTSubject
@@ -26,11 +27,18 @@ class UserModel extends Authenticatable implements JWTSubject
     protected $table = 'm_user'; //Mendefinisikan nama tabel yang digunakan oleh model ini
     protected $primaryKey = 'user_id'; //mendefinisikan primary key dari tabel yang dignakan
     //protected $fillable = ['level_id', 'username', 'nama', 'password'];
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    protected $fillable = ['level_id', 'username', 'nama', 'password', 'image'];
 
     public function level():BelongsTo{
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
+
+    protected function image(): Attribute 
+    { 
+        return Attribute::make( 
+            get: fn ($image) => url('/storage/posts/' . $image),        
+        ); 
+    } 
 
     public function stok(): HasMany{
         return $this->hasMany(StokModel::class, 'user_id', 'user_id');
