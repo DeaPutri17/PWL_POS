77c814f85e7a0f2914cdeef0205303fd25591859
DeaPutri17 <putrinastitidea@gmail.com>
2024-06-01 23:25:38 +0700

    jobsheet 12-Praktikum

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BarangController;
 use App\Http\Controllers\DetailPenjualanController;
+use App\Http\Controllers\FileUploadController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\ManagerController;
@@ -147,3 +148,6 @@ Route::group (['middleware' => ['auth']], function () {
         Route:: resource('manager', ManagerController::class);
     });
 });
+
+Route::get('/file-upload', [FileUploadController::class, 'fileUpload']);
+Route::post('/file-upload', [FileUploadController::class, 'prosesFileUpload']);
